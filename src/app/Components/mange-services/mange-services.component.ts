21e76fcba68a817a077db8bd1fe4bb6273d360b7
moudy99 <bobmoudy1@gmail.com>
2024-06-18 09:46:35 +0300

    add interfaces for services

--- src/app/Components/mange-services/mange-services.component.ts
+++ src/app/Components/mange-services/mange-services.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { GetServicesService } from 'src/app/Service/get-services.service';
 @Component({
   selector: 'app-mange-services',
   templateUrl: './mange-services.component.html',
-  styleUrls: ['./mange-services.component.css']
+  styleUrls: ['./mange-services.component.css'],
 })
 export class MangeServicesComponent {
-
+  constructor(private getServices: GetServicesService) {}
+  getAllServices() {
+    this.getServices.getAllServices().subscribe((response: any) => {
+      console.log(response);
+    });
+  }
 }
